d7701ff7a4890eb9513a343b37c67b0208c0f99e
Christopher Dorst <chris@dorst.email>
2018-04-30 10:55:05 -0500

    Refactor CsvActionResult using base type

--- Server/Internals/ActionResults/CsvActionResult.cs
+++ Server/Internals/ActionResults/CsvActionResult.cs
@@ -1,30 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using static Microsoft.AspNetCore.Http.StatusCodes;
-using static Server.Internals.ActionResults.ByteOffsetConstant;
 using static Server.Internals.ActionResults.ContentTypeConstants;
 using static System.Text.Encoding;
 
 namespace Server.Internals.ActionResults
 {
-    internal class CsvActionResult : IActionResult
+    internal class CsvActionResult : ActionResultBase
     {
-        private readonly byte[] _payload;
-        private readonly int _payloadLength;
-
-        public CsvActionResult(string csv)
+        public CsvActionResult(string content) : base(UTF8.GetBytes(content))
         {
-            _payload = UTF8.GetBytes(csv);
-            _payloadLength = _payload.Length;
         }
 
-        public Task ExecuteResultAsync(ActionContext context)
+        protected override HttpResponse GetResponse(ActionContext context)
         {
             var response = context.HttpContext.Response;
             response.StatusCode = Status200OK;
-            response.ContentType = CSV;
             response.ContentLength = _payloadLength;
-            return response.Body.WriteAsync(_payload, OffsetZero, _payloadLength);
+            response.ContentType = CSV;
+            return response;
         }
     }
 }

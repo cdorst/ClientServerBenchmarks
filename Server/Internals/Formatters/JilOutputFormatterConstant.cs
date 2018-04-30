9c181f9a08276ee7ef711c520ba1322ebe8a2dc6
Christopher Dorst <chris@dorst.email>
2018-04-30 10:58:26 -0500

    Remove Formatter types (use ActionResult types instead)

--- Server/Internals/Formatters/JilOutputFormatterConstant.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc.Formatters;
-
-namespace Server.Internals.Formatters
-{
-    internal static class JilOutputFormatterConstant
-    {
-        public static readonly IOutputFormatter JilFormatter = new JilOutputFormatter();
-    }
-}

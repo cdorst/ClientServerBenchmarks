b00de53c11363c81475f51fd14e3e7a3fdc1a24e
Christopher Dorst <chris@dorst.email>
2018-04-30 10:57:57 -0500

    Consolidate JsonActionResult & w/o nulls behavior; use base type

--- Server/Internals/ActionResults/JsonActionResult.cs
+++ Server/Internals/ActionResults/JsonActionResult.cs
@@ -1,31 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using static Jil.JSON;
 using static Microsoft.AspNetCore.Http.StatusCodes;
-using static Server.Internals.ActionResults.ByteOffsetConstant;
 using static Server.Internals.ActionResults.ContentTypeConstants;
+using static Server.Internals.ActionResults.JilOptionsConstants;
 using static System.Text.Encoding;
 
 namespace Server.Internals.ActionResults
 {
-    internal class JsonActionResult : IActionResult
+    internal class JsonActionResult : ActionResultBase
     {
-        private readonly byte[] _payload;
-        private readonly int _payloadLength;
-
         public JsonActionResult(object @object)
+            : base(UTF8.GetBytes(Serialize(@object, JilOptions)))
         {
-            _payload = UTF8.GetBytes(Serialize(@object));
-            _payloadLength = _payload.Length;
         }
 
-        public Task ExecuteResultAsync(ActionContext context)
+        protected override HttpResponse GetResponse(ActionContext context)
         {
             var response = context.HttpContext.Response;
             response.StatusCode = Status200OK;
-            response.ContentType = JSON;
             response.ContentLength = _payloadLength;
-            return response.Body.WriteAsync(_payload, OffsetZero, _payloadLength);
+            response.ContentType = JSON;
+            return response;
         }
     }
 }

5335c00ffe91a445cd6fe16a20e533ab46a481b5
Christopher Dorst <chris@dorst.email>
2018-04-30 10:54:15 -0500

    Add ActionResultBase class

---
+++ Server/Internals/ActionResults/ActionResultBase.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using static Microsoft.AspNetCore.Http.StatusCodes;
+using static Server.Internals.ActionResults.ByteOffsetConstant;
+
+namespace Server.Internals.ActionResults
+{
+    internal abstract class ActionResultBase : IActionResult
+    {
+        private readonly byte[] _payload;
+        protected readonly int _payloadLength;
+
+        public ActionResultBase(byte[] payload)
+        {
+            _payload = payload;
+            _payloadLength = _payload.Length;
+        }
+
+        public Task ExecuteResultAsync(ActionContext context)
+            => GetResponse(context)
+                .Body
+                .WriteAsync(_payload, OffsetZero, _payloadLength);
+
+        protected virtual HttpResponse GetResponse(ActionContext context)
+        {
+            var response = context.HttpContext.Response;
+            response.StatusCode = Status200OK;
+            response.ContentLength = _payloadLength;
+            return response;
+        }
+    }
+}

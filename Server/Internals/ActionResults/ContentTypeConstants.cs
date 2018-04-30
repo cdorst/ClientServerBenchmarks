7b1d4a88412802de435a721968c70db4974942a8
Christopher Dorst <chris@dorst.email>
2018-04-30 10:57:26 -0500

    Remove CsvActionResult (use Plaintext instead)

--- Server/Internals/ActionResults/ContentTypeConstants.cs
+++ Server/Internals/ActionResults/ContentTypeConstants.cs
@@ -2,7 +2,6 @@
 {
     internal static class ContentTypeConstants
     {
-        public const string CSV = "text/csv";
         public const string JSON = "application/json";
         public const string Plaintext = "text/plain";
     }

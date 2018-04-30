16bbba6491c71853440dae37f83520dd787b3ac9
Christopher Dorst <chris@dorst.email>
2018-04-30 10:55:32 -0500

    Add PlaintextActionResult

---
+++ Server/Internals/ActionResults/PlaintextActionResult.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using static Microsoft.AspNetCore.Http.StatusCodes;
+using static Server.Internals.ActionResults.ContentTypeConstants;
+using static System.Text.Encoding;
+
+namespace Server.Internals.ActionResults
+{
+    internal class PlaintextActionResult : ActionResultBase
+    {
+        public PlaintextActionResult(string content) : base(UTF8.GetBytes(content))
+        {
+        }
+
+        protected override HttpResponse GetResponse(ActionContext context)
+        {
+            var response = context.HttpContext.Response;
+            response.StatusCode = Status200OK;
+            response.ContentLength = _payloadLength;
+            response.ContentType = Plaintext;
+            return response;
+        }
+    }
+}
